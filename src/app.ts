d69ce6ad613bdf283279e5fd553c14c0ac6752a5
zolo1917 <adwaith_venkatesh@msn.com>
2024-08-19 18:03:05 +0530

    udpate ssl certificate

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import { getLogger, loadConfig } from "./config/config";
 import { roleRouter } from "./api/role";
 import { authRouter } from "./api/auth";
 import { userRouter } from "./api/user";
+import https from "https";
+import fs from "fs";
 import cors from "cors";
 import { initializieFirebaseApp } from "./config/firebaseConfig";
 // import { initializieFirebaseApp } from "./config/dbconfig";
@@ -24,6 +26,16 @@ const port = process.env.PORT || 3000;
 
 app.get("/", async (req: Request, res: Response) => {
   res.send({ message: "The User/Auth Service is up" });
+});
+console.log(fs.readFileSync(__dirname + "/server.key", "utf-8"));
+const httpsCredentials = {
+  key: fs.readFileSync(__dirname + "/server.key", "utf-8"),
+  cert: fs.readFileSync(__dirname + "/server.cert", "utf-8"),
+};
+const httpsServer = https.createServer(httpsCredentials, app);
+const securePort = process.env.SECURE_PORT;
+httpsServer.listen(securePort, () => {
+  logger.info(`Https server is running on port ${securePort}`);
 });
 app.listen(port, () => {
   logger.info(`Application is running on Port : ${port}`);
